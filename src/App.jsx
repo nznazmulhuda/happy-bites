0c52ed48667f4ee26daef8bfd4c23069f4bc6443
Nazmul Huda <nznazmulhdua04@gmail.com>
2024-03-16 14:58:02 +0600

    Ready to launch

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 				<OurRecipes />
 			</div>
 
-			<div className="border-t-2 mt-3 lg:mt-6 rounded-xl lg:rounded-t-[60px] lg:section pb-10">
+			<div className="border-t-2 mt-3 lg:mt-6 rounded-xl lg:rounded-t-[60px] section pb-10">
 				<RecipesSec />
 			</div>
 
